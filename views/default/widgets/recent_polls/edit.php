0f247c568ff1a8af19b1cbc4368b67fe9f155057
Jerome Bakker <jeabakker@coldtrick.com>
2015-09-23 16:10:06 +0200

    added: widgets/group_module with support for widget_manager

--- views/default/widgets/recent_polls/edit.php
+++ views/default/widgets/recent_polls/edit.php
@@ -11,7 +11,7 @@ $input = elgg_format_element('label', [], elgg_echo('widget:numbertodisplay'));
 $input .= elgg_view('input/select', [
 	'name' => 'params[num_display]',
 	'value' => $num_display,
-	'options' => range(1,10),
+	'options' => range(1, 10),
 	'class' => 'mls'
 ]);
 
