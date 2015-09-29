b159ad9dac3a20f9553df4861908d522984c0f19
Jerome Bakker <jeabakker@coldtrick.com>
2015-09-29 16:53:28 +0200

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file was created by Translation Editor v4.0
+ * On 2015-09-29 16:52
+ */
+
+return array (
+  'item:object:poll' => 'Peiling',
+  'river:create:object:default' => '%s heeft de peiling \'%s\' aangemaakt',
+  'river:vote:object:default' => '%s heeft gestemd op de peiling %s',
+  'poll:settings:enable_site' => 'Schakel peilingen in voor de site',
+  'poll:settings:enable_site:info' => 'Mogen gebruikers persoonlijke peilingen aanmaken?',
+  'poll:settings:enable_group' => 'Schakel peilingen in voor groepen',
+  'poll:settings:enable_group:info' => 'Mogen gebruikers peilingen aanmaken in groepen? Groepen moeten dit nog wel inschakelen om er gebruik van te kunnen maken.',
+  'poll:none' => 'Geen peilingen gevonden',
+  'poll:group' => 'Groepspeilingen',
+  'poll:add' => 'Maak een nieuwe peiling',
+  'poll:all:title' => 'Alle site peilingen',
+  'poll:owner:title' => 'Peilingen van %s',
+  'poll:friends:title' => 'Peilingen van vrienden',
+  'poll:edit:title' => 'Bewerk peiling \'%s\'',
+  'poll:edit:answers' => 'Antwoorden',
+  'poll:edit:answers:name' => 'Naam',
+  'poll:edit:answers:label' => 'Label',
+  'poll:edit:close_date' => 'Sluitdatum (optioneel)',
+  'poll:edit:error:cant_edit' => 'Je mag deze peiling niet bewerken',
+  'poll:vote:title' => 'Stem op deze peiling',
+  'poll:vote' => 'Stem',
+  'poll:menu:site' => 'Peilingen',
+  'poll:menu:owner_block:group' => 'Groepspeilingen',
+  'poll:menu:poll_tabs:vote' => 'Stem',
+  'poll:menu:poll_tabs:pie' => 'Resultaten taartdiagram',
+  'poll:menu:poll_tabs:bar' => 'Resultaten staafdiagram',
+  'poll:group_tool:title' => 'Groepspeilingen inschakelen',
+  'poll:widgets:single_poll:title' => 'Uitgelichte peiling',
+  'poll:widgets:single_poll:description' => 'Toon een enkele peiling',
+  'poll:widgets:single_poll:poll_guid' => 'Geef de GUID op van de peiling welke moet worden getoond in de widget',
+  'poll:widgets:single_poll:misconfigured' => 'De widget moet nog geconfigureerd worden',
+  'poll:widgets:recent_polls:title' => 'Recente peilingen',
+  'poll:widgets:recent_polls:description' => 'Toon een lijst met de recent aangemaakte peilingen',
+  'poll:container_gatekeeper:user' => 'Peilingen is niet ingeschakeld voor persoonlijk gebruik',
+  'poll:container_gatekeeper:group' => 'Peilingen is niet ingeschakeld voor gebruik in groepen',
+  'poll:notification:create:subject' => 'Een nieuwe peiling \'%s\' is aangemaakt',
+  'poll:notification:create:summary' => 'Nieuwe peiling \'%s\'',
+  'poll:notification:create:body' => 'Hallo,
+
+%s heeft de peiling \'%s\' aangemaakt.
+
+%s
+
+Om de peiling te bekijken klik op de link:
+%s',
+  'poll:action:edit:error:title' => 'Geef een titel op',
+  'poll:action:vote:error:input' => 'Je moet een antwoord kiezen',
+  'poll:action:vote:error:can_vote' => 'Je mag niet stemmen op deze peiling',
+  'poll:action:vote:error:vote' => 'Er is een fout opgetreden tijdens het opslaan van je stem',
+  'poll:action:vote:success' => 'Je stem is opgeslagen',
+);
