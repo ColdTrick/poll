407eb38abcf8ca54c56d25968861f149fd59daad
Jerome Bakker <jeabakker@coldtrick.com>
2016-02-09 16:04:41 +0100

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,40 @@
 <?php
 /**
- * This file was created by Translation Editor v4.0
- * On 2015-11-24 15:22
+ * This file was created by Translation Editor v4.0.1
+ * On 2016-02-09 16:04
  */
 
 return array (
+  'poll:settings:group_create' => 'Wie mogen er standaard peilingen aanmaken in de groep?',
+  'poll:settings:group_create:info' => 'Als groepspeilingen wordt ingeschakeld, beïnvloed deze instelling wie er standaard peilingen mogen aanmaken in de groep. De groepseigenaar kan dit aanpassen.',
+  'poll:settings:group_create:options:members' => 'Groepsleden',
+  'poll:settings:group_create:options:owners' => 'Groepseigenaren',
+  'poll:settings:close_date_required' => 'Is een sluitdatum verplicht?',
+  'poll:settings:close_date_required:info' => 'Standaard is een sluitdatum niet verplicht bij een peiling. Deze instelling maakt het verplicht.',
+  'poll:edit:answers:show_internal_names' => 'Toon interne namen',
+  'poll:edit:results_output' => 'Toon resultaten als',
+  'poll:edit:results_output:pie' => 'taart grafiek',
+  'poll:edit:results_output:bar' => 'balk grafiek',
+  'poll:edit:error:answer_count' => 'Een peiling moet minimaal twee antwoorden hebben',
+  'poll:closed' => 'Deze peiling is gesloten sinds:',
+  'poll:closed:future' => 'Peiling sluit:',
+  'poll:menu:poll_tabs:results' => 'Resultaten',
+  'poll:notification:close:owner:subject' => 'Je peiling "%s" is nu gesloten',
+  'poll:notification:close:owner:summary' => 'Je peiling "%s" is nu gesloten',
+  'poll:notification:close:owner:body' => 'Hallo %s,
+
+Je peiling "%s" is nu gesloten. Gebruikers kunnen niet langer hun stem uitbrengen.
+
+Om de resultaten van de peiling te bekijken, klik hier:
+%s',
+  'poll:notification:close:participant:subject' => 'De peiling "%s" waarop je hebt gestemd is nu gesloten',
+  'poll:notification:close:participant:summary' => 'De peiling "%s" waarop je hebt gestemd is nu gesloten',
+  'poll:notification:close:participant:body' => 'Hallo,
+
+De peiling "%s" waarop je hebt gestemd is nu gesloten.
+
+Om de resultaten van de peiling te bekijken, klik hier:
+%s',
   'poll:group_settings:title' => 'Peilingen instellingen voor groepsleden',
   'poll:group_settings:members' => 'Mogen groepsleden peilingen aanmaken',
   'poll:group_settings:members:description' => 'Als deze instelling op \'nee\' wordt gezet kunnen alleen groep eigenaren/beheerders peilingen aanmaken in deze groep.',
@@ -32,8 +62,6 @@ return array (
   'poll:menu:site' => 'Peilingen',
   'poll:menu:owner_block:group' => 'Groepspeilingen',
   'poll:menu:poll_tabs:vote' => 'Stem',
-  'poll:menu:poll_tabs:pie' => 'Resultaten taartdiagram',
-  'poll:menu:poll_tabs:bar' => 'Resultaten staafdiagram',
   'poll:group_tool:title' => 'Groepspeilingen inschakelen',
   'poll:widgets:single_poll:title' => 'Uitgelichte peiling',
   'poll:widgets:single_poll:description' => 'Toon een enkele peiling',
