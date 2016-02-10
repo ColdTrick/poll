cc2e5adeb85e471f95f34f149bad75c4a05dc8b4
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-02-10 10:29:07 +0100

    fixed: always show datepicker on edit form above the field

--- views/default/js/poll/edit.js
+++ views/default/js/poll/edit.js
@@ -48,6 +48,26 @@ elgg.poll.init_edit = function() {
 		
 		return false;
 	});
+	
+	$.extend($.datepicker,{_checkOffset:function(inst,offset,isFixed) {
+		var dpWidth = inst.dpDiv.outerWidth();
+		var dpHeight = inst.dpDiv.outerHeight();
+		var inputWidth = inst.input ? inst.input.outerWidth() : 0;
+		var inputHeight = inst.input ? inst.input.outerHeight() : 0;
+		var viewWidth = document.documentElement.clientWidth + (isFixed ? 0 : $(document).scrollLeft());
+		var viewHeight = document.documentElement.clientHeight + (isFixed ? 0 : $(document).scrollTop());
+
+		offset.left -= (this._get(inst, 'isRTL') ? (dpWidth - inputWidth) : 0);
+		offset.left -= (isFixed && offset.left == inst.input.offset().left) ? $(document).scrollLeft() : 0;
+		offset.top -= (isFixed && offset.top == (inst.input.offset().top + inputHeight)) ? $(document).scrollTop() : 0;
+
+		// now check if datepicker is showing outside window viewport - move to a better place if so.
+		offset.left -= Math.min(offset.left, (offset.left + dpWidth > viewWidth && viewWidth > dpWidth) ?
+			Math.abs(offset.left + dpWidth - viewWidth) : 0);
+		offset.top -= Math.abs(dpHeight + inputHeight);
+
+		return offset;
+	}});
 };
 
 //register init hook
