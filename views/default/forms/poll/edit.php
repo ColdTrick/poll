b0c7c0c7c148e380c3e6ed58fed08d285f119ee7
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-03-08 15:10:02 +0100

    chore: updated edit form to use elgg_view_field

--- views/default/forms/poll/edit.php
+++ views/default/forms/poll/edit.php
@@ -10,100 +10,99 @@ $noyes_options = [
 	'yes' => elgg_echo('option:yes'),
 ];
 
-$results_output_options = [
-	'pie' => elgg_echo('poll:edit:results_output:pie'),
-	'bar' => elgg_echo('poll:edit:results_output:bar'),
-];
-
 // build form elements
 // title
-$title = elgg_format_element('label', ['for' => 'poll-title'], elgg_echo('title'));
-$title .= elgg_view('input/text', [
+echo elgg_view_field([
+	'#type' => 'text',
+	'#label' => elgg_echo('title'),
 	'name' => 'title',
 	'value' => elgg_extract('title', $vars),
 	'id' => 'poll-title',
 	'required' => true,
 ]);
-echo elgg_format_element('div', [], $title);
 
 // description
-$description = elgg_format_element('label', ['for' => 'poll-description'], elgg_echo('description'));
-$description .= elgg_view('input/longtext', [
+echo elgg_view_field([
+	'#type' => 'longtext',
+	'#label' => elgg_echo('description'),
 	'name' => 'description',
 	'value' => elgg_extract('description', $vars),
 	'id' => 'poll-description',
 ]);
-echo elgg_format_element('div', [], $description);
 
 // answers
-$answers = elgg_format_element('label', [], elgg_echo('poll:edit:answers'));
+$answers = elgg_view('elements/forms/label', ['label' => elgg_echo('poll:edit:answers'), 'required' => true]);;
 $answers .= elgg_view('poll/edit/answers', $vars);
 echo elgg_format_element('div', [], $answers);
 
 // close date
-$answers = elgg_format_element('label', ['for' => 'close_date'], elgg_echo('poll:edit:close_date'));
-$answers .= elgg_view('input/date', [
+echo elgg_view_field([
+	'#type' => 'date',
+	'#label' => elgg_echo('poll:edit:close_date'),
 	'name' => 'close_date',
 	'value' => elgg_extract('close_date', $vars),
 	'timestamp' => true,
 	'required' => poll_get_plugin_setting('close_date_required') === 'yes' ? true : false,
 ]);
-echo elgg_format_element('div', [], $answers);
 
 // tags
-$tags = elgg_format_element('label', ['for' => 'poll-tags'], elgg_echo('tags'));
-$tags .= elgg_view('input/tags', [
+echo elgg_view_field([
+	'#type' => 'tags',
+	'#label' => elgg_echo('tags'),
+	'id' => 'poll-tags',
 	'name' => 'tags',
 	'value' => elgg_extract('tags', $vars),
-	'id' => 'poll-tags',
 ]);
-echo elgg_format_element('div', [], $tags);
 
 // results output
-$comments = elgg_format_element('label', ['for' => 'poll-results-output'], elgg_echo('poll:edit:results_output'));
-$comments .= elgg_view('input/select', [
-	'name' => 'results_output',
-	'options_values' => $results_output_options,
-	'value' => elgg_extract('results_output', $vars),
+echo elgg_view_field([
+	'#type' => 'select',
+	'#label' => elgg_echo('poll:edit:results_output'),
 	'id' => 'poll-results-output',
-	'class' => 'mls',
+	'name' => 'results_output',
+	'options_values' => [
+		'pie' => elgg_echo('poll:edit:results_output:pie'),
+		'bar' => elgg_echo('poll:edit:results_output:bar'),
+	],
+	'value' => elgg_extract('results_output', $vars),
 ]);
-echo elgg_format_element('div', [], $comments);
 
 // comments
-$comments = elgg_format_element('label', ['for' => 'poll-comments'], elgg_echo('comments'));
-$comments .= elgg_view('input/select', [
+echo elgg_view_field([
+	'#type' => 'select',
+	'#label' => elgg_echo('comments'),
+	'id' => 'poll-comments',
 	'name' => 'comments_allowed',
 	'options_values' => $noyes_options,
 	'value' => elgg_extract('comments_allowed', $vars),
-	'id' => 'poll-comments',
-	'class' => 'mls',
 ]);
-echo elgg_format_element('div', [], $comments);
 
 // access
-$access = elgg_format_element('label', ['for' => 'poll-access-id'], elgg_echo('access'));
-$access .= elgg_view('input/access', [
+echo elgg_view_field([
+	'#type' => 'access',
+	'#label' => elgg_echo('access'),
 	'name' => 'access_id',
 	'value' => elgg_extract('access_id', $vars),
 	'id' => 'poll-access-id',
 	'entity' => $entity,
 	'entity_type' => 'object',
 	'entity_subtype' => Poll::SUBTYPE,
-	'class' => 'mls',
 ]);
-echo elgg_format_element('div', [], $access);
 
-// footer
-$footer = elgg_view('input/hidden', [
+echo elgg_view_field([
+	'#type' => 'hidden',
 	'name' => 'guid',
 	'value' => (int) elgg_extract('guid', $vars),
 ]);
-$footer .= elgg_view('input/hidden', [
+echo elgg_view_field([
+	'#type' => 'hidden',
 	'name' => 'container_guid',
 	'value' => (int) elgg_extract('container_guid', $vars),
 ]);
-$footer .= elgg_view('input/submit', [
+
+// footer
+$footer = elgg_view_field([
+	'#type' => 'submit',
 	'value' => elgg_echo('save'),
 ]);
-echo elgg_format_element('div', ['class' => 'elgg-foot'], $footer);
+elgg_set_form_footer($footer);
