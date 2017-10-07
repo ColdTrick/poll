14d7f39ca0114c81aa579d3a31d2659d653037f4
dragonjon <jonaltuna@msn.com>
2017-10-07 17:25:16 +0200

    Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,94 @@
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 17:21
+ */
+
+return array (
+  'poll:settings:group_create:info' => 'Al habilitar encuestas del grupo, este ajuste controla quién puede crear encuestas de forma predeterminada. Los propietarios de grupos pueden configurar de otra manera.',
+  'poll:menu:owner_block:group' => 'Encuestas del grupo',
+  'poll:group_tool:title' => 'Habilitar encuestas del grupo',
+  'item:object:poll' => 'Encuesta',
+  'river:create:object:poll' => '%s se ha creado una nueva encuesta %s',
+  'river:vote:object:poll' => '%s votado en la encuesta %s',
+  'poll:settings:enable_site' => 'Habilitar encuestas para el sitio',
+  'poll:settings:enable_site:info' => '¿Se les permite a los usuarios crear encuestas personales?',
+  'poll:settings:enable_group' => 'Habilitar encuestas para grupos',
+  'poll:settings:enable_group:info' => '¿Se les permite a los usuarios crear encuestas en grupos? Grupos todavía tiene que activar esta función si desean utilizarlo.',
+  'poll:settings:group_create' => 'Por defecto, ¿quién puede crear encuestas en un grupo?',
+  'poll:settings:group_create:options:members' => 'Miembros de grupo',
+  'poll:settings:group_create:options:owners' => 'Propietarios de grupo',
+  'poll:settings:close_date_required' => '¿Fecha de cierre requerido?',
+  'poll:settings:close_date_required:info' => 'De manera predeterminada, no se requiere una fecha de cierre para una encuesta. Esta configuración hará que sea necesario.',
+  'poll:settings:vote_change_allowed' => '¿Está permitido cambiar su voto?',
+  'poll:settings:vote_change_allowed:info' => 'Esta configuración permite a los usuarios cambiar su voto después de que hayan votado.',
+  'poll:settings:add_vote_to_river' => 'Añadir la actividad de voto de encuesta a Actividad',
+  'poll:settings:add_vote_to_river:info' => 'Esta configuración determina si se añade la votación de una encuesta para el flujo de actividad.',
+  'poll:none' => 'Encuesta no encontrada',
+  'poll:group' => 'Encuesta de grupo',
+  'poll:add' => 'Añadir encuesta',
+  'poll:all:title' => 'Todas las encuestas del sitio',
+  'poll:owner:title' => 'Encuestas de %s',
+  'poll:friends:title' => 'Encuestas de amigos',
+  'poll:edit:title' => 'Editar encuesta "%s"',
+  'poll:edit:answers' => 'Respuestas',
+  'poll:edit:answers:name' => 'Nombre',
+  'poll:edit:answers:label' => 'Etiqueta',
+  'poll:edit:answers:show_internal_names' => 'Mostrar nombres internos',
+  'poll:edit:close_date' => 'Fecha de cierre',
+  'poll:edit:results_output' => 'Enseñar resultados de encuesta como',
+  'poll:edit:results_output:pie' => 'gráfico de sectores',
+  'poll:edit:results_output:bar' => 'gráfico de barras',
+  'poll:edit:error:cant_edit' => 'Usted no tiene permiso para editar esta encuesta',
+  'poll:edit:error:answer_count' => 'Se necesita al menos dos respuestas al crear una encuesta',
+  'poll:closed' => 'Esta encuesta está cerrada desde:',
+  'poll:closed:future' => 'Se cierra la encuesta:',
+  'poll:vote:title' => 'Votar en esta encuesta',
+  'poll:vote' => 'Votar',
+  'poll:menu:site' => 'Encuestas',
+  'poll:menu:poll_tabs:vote' => 'Votar',
+  'poll:menu:poll_tabs:results' => 'Resultados',
+  'poll:group_settings:title' => 'Configuración de encuesta de miembro de grupo',
+  'poll:group_settings:members' => 'Permitir a los miembros del grupo crear encuestas',
+  'poll:group_settings:members:description' => 'Cuando este valor se establece en \'no\', únicamente los propietarios / administradores pueden crear encuestas en este grupo.',
+  'poll:widgets:single_poll:title' => 'Encuesta destacada',
+  'poll:widgets:single_poll:description' => 'Mostrar un sola encuesta',
+  'poll:widgets:single_poll:poll_guid:object' => 'Introduzca el título de la encuesta y seleccione de la lista',
+  'poll:widgets:single_poll:poll_guid:guid' => 'Introduzca el GUID de la encuesta que se debe mostrar en el widget',
+  'poll:widgets:single_poll:misconfigured' => 'Es necesario configurar este widget',
+  'poll:widgets:recent_polls:title' => 'Encuestas reciente',
+  'poll:widgets:recent_polls:description' => 'Mostrar una lista de las encuestas recientes',
+  'poll:container_gatekeeper:user' => 'Encuesta no está habilitado para uso personal',
+  'poll:container_gatekeeper:group' => 'Encuesta no está habilitado para uso en grupos',
+  'poll:notification:create:subject' => 'Una nueva encuesta "%s" se ha creado',
+  'poll:notification:create:summary' => 'Nueva encuesta "%s"',
+  'poll:notification:create:body' => 'Hola,
+
+%s Creada nueva encuesta "%s".
+
+%s
+
+Para ver la encuesta pulse el enlace:
+%s',
+  'poll:notification:close:owner:subject' => 'Su encuesta "%s" está cerrada',
+  'poll:notification:close:owner:summary' => 'Su encuesta "%s" está cerrada',
+  'poll:notification:close:owner:body' => 'Hola %s,
+
+Su encuesta "%s" esta cerrada. Los usuarios ya no pueden votar.
+
+Para ver los resultados echa un vistazo a la encuesta aquí:
+%s',
+  'poll:notification:close:participant:subject' => 'La encuesta "%s" en la que ha votado esta cerrada',
+  'poll:notification:close:participant:summary' => 'La encuesta "%s" en la que ha votado esta cerrada',
+  'poll:notification:close:participant:body' => 'Hola,
+
+La encuesta "%s" en la que ha votado esta cerrada.
+
+Para ver los resultados echa un vistazo a la encuesta aquí:
+%s',
+  'poll:action:edit:error:title' => 'Por favor proporcione un título',
+  'poll:action:vote:error:input' => 'Necesita seleccionar una respuesta',
+  'poll:action:vote:error:can_vote' => 'No tiene permitido votar en esta encuesta',
+  'poll:action:vote:error:vote' => 'Un error ha ocurrido al guardar su voto',
+  'poll:action:vote:success' => 'Un error ha ocurrido al guardar su voto',
+);
