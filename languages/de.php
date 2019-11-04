6aa77ae6da038af6456020b4f379d4a8ef643019
Dennis Ploeger <github.dennis@dieploegers.de>
2019-11-04 11:50:47 +0100

    added: german translation

---
+++ languages/de.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0.4
+ * On 2019-10-31 17:32
+ */
+
+return array (
+  'item:object:poll' => 'Umfrage',
+  'collection:object:poll' => 'Umfragen',
+  'river:object:poll:create' => '%s hat eine neue Umfrage "%s" erstellt',
+  'river:object:poll:vote' => '%s hat an der Umfrage "%s" teilgenommen',
+  'river:object:poll:comment' => '%s hat die Umfrage %s" kommentiert',
+  'poll:settings:enable_site' => 'Umfragen für diese Site aktivieren',
+  'poll:settings:enable_site:info' => 'Dürfen Benutzer persönliche Umfragen erstellen?',
+  'poll:settings:enable_group' => 'Umfragen für Gruppen aktivieren',
+  'poll:settings:enable_group:info' => 'Können Benutzer Umfragen in Gruppen erzeugen? Gruppen müssen dieses Feature zusätzlich aktivieren, wenn sie es nutzen wollen.',
+  'poll:settings:group_create' => 'Wer kann standardmäßig eine Umfrage in einer Gruppe starten?',
+  'poll:settings:group_create:info' => 'Wenn Gruppenumfragen aktiviert sind, wird hier eingestellt, wer Umfragen standardmäßig erstellen kann. Gruppenbesitzer können dies umkonfigurieren.',
+  'poll:settings:group_create:options:members' => 'Gruppenmitglieder',
+  'poll:settings:group_create:options:owners' => 'Gruppenbesitzer',
+  'poll:settings:close_date_required' => 'Abschlussdatum erforderlich?',
+  'poll:settings:close_date_required:info' => 'Standardmäßig ist ein Abschlussdatum nicht erforderlich für eine Umfrage. Diese Einstellung wird es erforderlich machen.',
+  'poll:settings:vote_change_allowed' => 'Ist es erlaubt, seine Stimmenabgabe nachträglich zu ändern?',
+  'poll:settings:vote_change_allowed:info' => 'Diese Einstellung erlaubt Benutzern ihre Wahl zu ändern nachdem sie abgestimmt haben.',
+  'poll:settings:add_vote_to_river' => 'Umfragenaktivität zur Aktivitätenliste hinzufügen',
+  'poll:settings:add_vote_to_river:info' => 'Diese Einstellung legt fest, ob das Abstimmen bei einer Umfrage zur Aktivitätenliste hinzugefügt wird.',
+  'poll:none' => 'Keine Umfragen gefunden',
+  'poll:group' => 'Gruppenumfragen',
+  'poll:add' => 'Eine neue Umfrage erstellen',
+  'poll:all:title' => 'Alle Site-Umfragen',
+  'poll:owner:title' => 'Umfragen von %s',
+  'poll:friends:title' => 'Umfragen der Freunde',
+  'poll:edit:title' => 'Umfrage "%s" bearbeiten',
+  'poll:edit:answers' => 'Antworten',
+  'poll:edit:answers:name' => 'Name',
+  'poll:edit:answers:label' => 'Beschriftung',
+  'poll:edit:answers:show_internal_names' => 'Interne Namen anzeigen',
+  'poll:edit:close_date' => 'Abschlussdatum',
+  'poll:edit:results_output' => 'Die Umfrageergebnisse anzeigen als',
+  'poll:edit:results_output:pie' => 'Kuchendiagramm',
+  'poll:edit:results_output:bar' => 'Balkendiagramm',
+  'poll:edit:error:cant_edit' => 'Sie dürfen diese Umfrage nicht bearbeiten',
+  'poll:edit:error:answer_count' => 'Sie müssen beim Erstellen einer Umfrage mindestens zwei Antworten angeben',
+  'poll:closed' => 'Diese Umfrage ist geschlossen seit:',
+  'poll:closed:future' => 'Die Umfrage schließt:',
+  'poll:no_votes' => 'Keine Stimmen wurden bisher abgegeben',
+  'poll:vote:title' => 'Bei dieser Umfrage abstimmen',
+  'poll:vote' => 'Abstimmen',
+  'poll:menu:site' => 'Umfragen',
+  'poll:menu:owner_block:group' => 'Gruppenumfragen',
+  'poll:menu:poll_tabs:vote' => 'Abstimmen',
+  'poll:menu:poll_tabs:results' => 'Ergebnisse',
+  'poll:group_tool:title' => 'Gruppenumfragen aktivieren',
+  'poll:group_settings:title' => 'Einstellungen für Mitglieder bei Gruppenumfragen',
+  'poll:group_settings:members' => 'Gruppenmitgliedern erlauben, Umfragen zu erzeugen',
+  'poll:group_settings:members:description' => 'Wenn diese Einstellung auf "nein" gesetzt ist, können nur Gruppenbesitzer und Admins Umfragen in dieser Gruppe erstellen.',
+  'widgets:single_poll:name' => 'Empfohlene Umfrage',
+  'widgets:single_poll:description' => 'Eine einzelne Umfrage anzeigen',
+  'poll:widgets:single_poll:poll_guid:object' => 'Geben Sie bitte den Umfragetitel ein und wählen ihn aus der Liste aus',
+  'widgets:recent_polls:name' => 'Letzte Umfragen',
+  'widgets:recent_polls:description' => 'Eine Liste der kürzlich angelegten Umfragen anzeigen',
+  'poll:container_gatekeeper:user' => 'Die Umfragefunktion wurde nicht für persönliche Umfragen aktiviert',
+  'poll:container_gatekeeper:group' => 'Die Umfragefunktion wurde nicht für Gruppenumfragen aktiviert',
+  'poll:notification:create:subject' => 'Eine neue Umfrage wurde erstellt: "%s"',
+  'poll:notification:create:summary' => 'Neue Umfrage "%s"',
+  'poll:notification:create:body' => 'Hallo,
+
+%s hat eine neue Umfrage "%s" erstellt.
+
+%s
+
+Um die Umfrage anzuzeigen, klicke auf diesen Link:
+%s',
+  'poll:notification:close:owner:subject' => 'Deine Umfrage "%s" wurde nun geschlossen',
+  'poll:notification:close:owner:summary' => 'Deine Umfrage "%s" wurde nun geschlossen',
+  'poll:notification:close:owner:body' => 'Hallo %s,
+
+Deine Umfrage "%s" ist nun geschlossen. Die Benutzer können nicht länger abstimmen.
+
+Um die Ergebnisse anzuzeigen, gehe auf die Umfrageseite:
+%s',
+  'poll:notification:close:participant:subject' => 'Die Umfrage "%s", bei der Du teilgenommen hast, ist geschlossen.',
+  'poll:notification:close:participant:summary' => 'Die Umfrage "%s", bei der Du teilgenommen hast, ist geschlossen.',
+  'poll:notification:close:participant:body' => 'Hallo,
+
+Die Umfrage "%s", bei der Du teilgenommen hast ist nun geschlossen.
+
+Um die Umfrageergebnisse anzuzeigen, besuche die Umfrageseite:
+%s',
+  'poll:action:edit:error:title' => 'Bitte geben Sie einen Titel an',
+  'poll:action:vote:error:input' => 'Sie müssen eine Antwort auswählen',
+  'poll:action:vote:error:can_vote' => 'Sie dürfen nicht bei dieser Umfrage abstimmen',
+  'poll:action:vote:error:vote' => 'Ein Fehler trat beim Speichern Ihrer Abstimmung auf',
+  'poll:action:vote:success' => 'Ihre Wahl wurde abgespeichert',
+  'poll:action:export:error:no_votes' => 'Keine Abstimmungen für den Export verfügbar',
+);
